bd40a27cf736a729bc082d844bca55f3a603518e
Pedro Suassuna <pedro_suassuna@hotmail.com>
2023-05-24 21:49:50 -0300

    3º commit - Eye mechanism addition + bar's animation correction with overflow

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let input1 = document.querySelector("#email")
-let input2 = document.querySelector("#password")
+let inputEmail = document.querySelector("#email")
+let inputPasword = document.querySelector("#password")
 let line1 = document.querySelector(".line.email")
 let line2 = document.querySelector(".line.password")
 
-input1.addEventListener("focus", change1)
-input1.addEventListener("blur", changeBack1)
+inputEmail.addEventListener("focus", change1)
+inputEmail.addEventListener("blur", changeBack1)
 
-input2.addEventListener("focus", change2)
-input2.addEventListener("blur", changeBack2)
+inputPasword.addEventListener("focus", change2)
+inputPasword.addEventListener("blur", changeBack2)
 
 function change1() {
   line1.classList.add("slide")
@@ -27,4 +27,22 @@ function change2() {
 function changeBack2() {
   line2.classList.remove("slide")
   line2.classList.add("slideReverse")
+}
+
+let eyeBlocked = document.querySelector(".closed")
+let eyeOpen = document.querySelector(".opened")
+
+eyeBlocked.addEventListener("click", openEye)
+eyeOpen.addEventListener("click", closeEye)
+
+function openEye() {
+  eyeBlocked.classList.add("hide")
+  eyeOpen.classList.remove("hide")
+  inputPasword.setAttribute("type", "text")
+}
+
+function closeEye() {
+  eyeBlocked.classList.remove("hide")
+  eyeOpen.classList.add("hide")
+  inputPasword.setAttribute("type", "password")
 }
